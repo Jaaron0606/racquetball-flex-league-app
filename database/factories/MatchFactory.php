abc13e18820631e730f3b14ca5fb8a598a90bccc
Creighton Magoun <magoun@gmail.com>
2018-06-24 10:53:54 -0500

    CM - Create Match factory

--- database/factories/MatchFactory.php
+++ database/factories/MatchFactory.php
@@ -4,6 +4,7 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Match::class, function (Faker $faker) {
     return [
-        //
+        'player_one' => $faker->name(),
+        'player_two' => $faker->name(),
     ];
 });
