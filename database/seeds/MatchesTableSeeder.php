702d36b2dc2d03572f55f3563edae0108690c8ee
Creighton Magoun <magoun@gmail.com>
2018-06-24 22:56:34 -0500

    CM - Debug database seeder

--- database/seeds/MatchesTableSeeder.php
+++ database/seeds/MatchesTableSeeder.php
@@ -11,10 +11,14 @@ class MatchesTableSeeder extends Seeder
      */
     public function run()
     {
-        $matches = factory(App\Match::class, 5)
-           ->create()
-           ->each(function ($match) {
-                $match->games()->save(factory(App\Game::class, 3)->make());
-            });
+        // $matches = factory(App\Match::class, 5)
+        //   ->create()
+        //   ->each(function ($match) {
+        //         $match->games()->save(factory(App\Game::class, 3)->make());
+        //     });
+        
+        $matches = factory(App\Match::class, 5)->make();
+        
+        echo $matches;
     }
 }
