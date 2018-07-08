198baa7a334a74a5591b9c48158ae763814fb31c
Creighton Magoun <magoun@gmail.com>
2018-07-08 14:49:03 -0500

    CM - Modify test data using PHPUnit tests

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -19,12 +19,43 @@ class ExampleTest extends TestCase
     
     public function testModelSeeders()
     {
-        $test = \App\Game::find(10)->match;
-        $player = $test->player_one;
-        // $test2 = \App\Game
+        $games = \App\Game::all();
+        
+        $games = $games->filter(function($game)
+        {
+            return ($game->id % 3) == 0;
+        });
+        
+        foreach ($games as $game) 
+        {
+            if ($game->player_one_score == 15) 
+            {
+                $game->player_one_score = 11;
+            } 
+            elseif ($game->player_two_score == 15)
+            {
+                $game->player_two_score = 11;
+            }
+            
+            $game->save();
+        }
+        
+        $games = \App\Game::all();
+        
+        foreach ($games as $game) 
+        {
+            echo PHP_EOL;
+            // echo $game->match->player_one;
+            // echo $game->match->player_two;
+            echo $game->player_one_score;
+            echo ' ';
+            echo $game->player_two_score;
+        }
+        
+        
         
         echo PHP_EOL;
-        echo $player;
+        echo $games->count();
         echo "\nSuccess\n";
         
         $this->assertTrue(true);
