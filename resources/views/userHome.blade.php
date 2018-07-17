9617d5cbfece876b920818f4d00d525a89d01508
Creighton Magoun <magoun@gmail.com>
2018-07-16 23:50:43 -0500

    Add scoreboard card layout to view

--- resources/views/userHome.blade.php
+++ resources/views/userHome.blade.php
@@ -1,91 +1,143 @@
 <!doctype html>
 <html lang="{{ app()->getLocale() }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Match Scores Update Form</title>
+    <title>Match Scores Update Form</title>
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+    <!-- Bootstrap Stuff -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/css/bootstrap.min.css" integrity="sha384-Smlep5jCw/wG7hdkwQ/Z5nLIefveQRIY9nfy6xoR1uRYBtpZgI6339F5dgvm/e9B" crossorigin="anonymous">
+    <link href="/css/style.css" rel="stylesheet">
+    
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/js/bootstrap.min.js" integrity="sha384-o+RDsa0aLu++PJvFqy8fFScvbHFLtbvScb8AjopnFD+iEQ7wo/CG0xlczd+2O/em" crossorigin="anonymous"></script>
+    
+    
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Raleway', sans-serif;
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
-            }
+    <!-- Styles -->
+    <style>
+      .content {
+        text-align: center;
+      }
 
-            .full-height {
-                height: 100vh;
-            }
+      .title {
+        font-size: 84px;
+      }
 
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
+      .m-b-md {
+        margin-bottom: 30px;
+      }
+    </style>
+  </head>
+  <body>
+    <div class="content">
+      <div class="title m-b-md">
+        Current League Performance
+      </div>
 
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="content">
-            <div class="title m-b-md">
-                Current League Performance
+      <!--
+        Scoreboard block: Shows the current standings for the logged in
+        users league, with the users name and point total highlighted.
+        
+        The scoreboard is sorted by current total points, highest 
+        on top.
+      -->
+      
+      <div class="container">
+        <div class="card-deck">
+          <div class="card border-primary mb-3">
+            <div class="card-header">Header</div>
+            <div class="card-body text-primary">
+              <h5 class="card-title">Primary card title</h5>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
             </div>
+          </div>
+          <div class="card border-secondary mb-3">
+            <div class="card-header">Header</div>
+            <div class="card-body text-secondary">
+              <h5 class="card-title">Secondary card title</h5>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+          </div>
+          <div class="card border-success mb-3">
+            <div class="card-header">Header</div>
+            <div class="card-body text-success">
+              <h5 class="card-title">Success card title</h5>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+          </div>
+          <div class="card border-danger mb-3">
+            <div class="card-header">Header</div>
+            <div class="card-body text-danger">
+              <h5 class="card-title">Danger card title</h5>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+          </div>
+          <div class="card border-warning mb-3">
+            <div class="card-header">Header</div>
+            <div class="card-body text-warning">
+              <h5 class="card-title">Warning card title</h5>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+          </div>
+          <div class="card border-info mb-3">
+            <div class="card-header">Header</div>
+            <div class="card-body text-info">
+              <h5 class="card-title">Info card title</h5>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+          </div>
 
-            <!--
-                Scoreboard block: Shows the current standings for the logged in
-                users league, with the users name and point total highlighted.
-                
-                The scoreboard is sorted by current total points, highest 
-                on top.
-            -->
-            
-            <!--
-                User match history block: Shows the users individual matches,
-                sorted by schedule week (earliest first). The user can see the
-                her match score and her opponent's match score, as well as an
-                edit button that links to the updateScores form.
-                
-                Matches without scores yet reported are clearly marked.
-            -->
+          <div class="card border-dark mb-3">
+            <div class="card-header border-dark">Open/A Singles</div>
+            <div class="card-body text-dark">
+              <table>
+                <tr class="card-title">
+                  <th>Player</th>
+                  <th>Score</th>
+                  <th>Games</th>
+                </tr>
+                <tr class="card-text">
+                  <td>Kooper</td>
+                  <td>162</td>
+                  <td>3 / 7</td>
+                </tr>
+                <tr class="card-text">
+                  <td>John</td>
+                  <td>121</td>
+                  <td>3 / 7</td>
+                </tr>
+                <tr class="card-text">
+                  <td>Freddy</td>
+                  <td>90</td>
+                  <td>2 / 7</td>
+                </tr>
+              </table>
+            </div>
+          </div>
+          <div class="card border-light mb-3">
+            <div class="card-header border-light">Header</div>
+            <div class="card-body text-dark">
+              <h5 class="card-title">Dark card title</h5>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+          </div>
         </div>
-    </body>
+      </div>
+
+      <!--
+        User match history block: Shows the users individual matches,
+        sorted by schedule week (earliest first). The user can see the
+        her match score and her opponent's match score, as well as an
+        edit button that links to the updateScores form.
+        
+        Matches without scores yet reported are clearly marked.
+      -->
+    </div>
+  </body>
 </html>
