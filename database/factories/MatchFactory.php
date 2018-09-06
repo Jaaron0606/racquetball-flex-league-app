001d3b51e1ebc5e316ecc7236c3fd22b1c3324d8
Creighton Magoun <magoun@gmail.com>
2018-09-05 22:30:53 -0500

    Modify database and factory structure to test views

--- database/factories/MatchFactory.php
+++ database/factories/MatchFactory.php
@@ -2,9 +2,11 @@
 
 use Faker\Generator as Faker;
 
-$factory->define(App\Match::class, function (Faker $faker) {
-    return [
-        'player_one' => $faker->name(),
-        'player_two' => $faker->name(),
-    ];
-});
+// Don't need this really...
+
+// $factory->define(App\Match::class, function (Faker $faker) {
+//     return [
+//         'player_one' => $faker->name(),
+//         'player_two' => $faker->name(),
+//     ];
+// });
