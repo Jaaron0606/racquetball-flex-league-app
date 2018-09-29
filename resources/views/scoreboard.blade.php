e4f5c2e653d320e953a9869effb1613269f1a0c2
Creighton Magoun <magoun@gmail.com>
2018-09-29 08:42:29 -0500

    Fix issue with Google trying to translate pages

--- resources/views/scoreboard.blade.php
+++ resources/views/scoreboard.blade.php
@@ -1,102 +1,88 @@
 @extends('layouts.app')
 
 @section('content')
-  <div class="content">
-    <div class="title m-b-md">
-      League Title
-    </div>
-  
-    <!--
-      Scoreboard block: Shows the current standings for the logged in
-      users league, with the users name and point total highlighted.
-      
-      The scoreboard is sorted by current total points, highest 
-      on top.
-    -->
+<div class="content">
+  <div class="title m-b-md">
+    League Title
+  </div>
+
+  <!--
+    Scoreboard block: Shows the current standings for the logged in
+    users league, with the users name and point total highlighted.
     
-    <div class="container" id="scoreboard">
-      <div class="card-deck">
-      
-        <?php
-          // $matches = \App\Match::all();
-          $leagueMatches = \App\Match::where('league_name', 'Test League')->get();
-          $leagueDivs = $leagueMatches->unique('division_name');
-          $divisions = Array();
+    The scoreboard is sorted by current total points, highest 
+    on top.
+  -->
+  
+  <div class="container" id="scoreboard">
+    <div class="card-deck">
+    
+      <?php
+        // $matches = \App\Match::all();
+        $leagueMatches = \App\Match::where('league_name', 'Test League')->get();
+        $leagueDivs = $leagueMatches->unique('division_name');
+        $divisions = Array();
+        
+        foreach ($leagueDivs as $leagueDiv) {
+          array_push($divisions, $leagueDiv->division_name);
+        }
+        
+        $scoreboard = Array();
+        
+        foreach ($divisions as $division) {
+          $matches = $leagueMatches->where('division_name', $division)->all();
+          $player_scores = Array();
           
-          foreach ($leagueDivs as $leagueDiv) {
-            array_push($divisions, $leagueDiv->division_name);
-          }
-          
-          $scoreboard = Array();
-          
-          foreach ($divisions as $division) {
-            $matches = $leagueMatches->where('division_name', $division)->all();
-            $player_scores = Array();
+          foreach ($matches as $match) {
+            $p1 = $match->player_one_id;
+            $p1_score = $match->player_one_total;
+            $p2 = $match->player_two_id;
+            $p2_score = $match->player_two_total;
             
-            foreach ($matches as $match) {
-              $p1 = $match->player_one_id;
-              $p1_score = $match->player_one_total;
-              $p2 = $match->player_two_id;
-              $p2_score = $match->player_two_total;
-              
-              if (array_key_exists($p1, $player_scores)) {
-                $player_scores[$p1] += $p1_score;
-              } else {
-                $player_scores[$p1] = $p1_score;
-              }
-              
-              if (array_key_exists($p2, $player_scores)) {
-                $player_scores[$p2] += $p2_score;
-              } else {
-                $player_scores[$p2] = $p2_score;
-              }
+            if (array_key_exists($p1, $player_scores)) {
+              $player_scores[$p1] += $p1_score;
+            } else {
+              $player_scores[$p1] = $p1_score;
             }
             
-            arsort($player_scores);
-            $scoreboard[$division] = $player_scores;
+            if (array_key_exists($p2, $player_scores)) {
+              $player_scores[$p2] += $p2_score;
+            } else {
+              $player_scores[$p2] = $p2_score;
+            }
           }
           
-          // dd($scoreboard);
-        ?>
+          arsort($player_scores);
+          $scoreboard[$division] = $player_scores;
+        }
         
-        @foreach ($scoreboard as $division_name => $division_totals)
-          <div class="card border-dark mb-3">
-            <div class="card-header border-dark">{{ $division_name }}</div>
-            <div class="card-body text-dark">
-              <table class="table table-sm">
-                <tr class="card-title">
-                  <th>#</th>
-                  <th>Player</th>
-                  <th>Points</th>
-                </tr>
-                
-                @foreach ($division_totals as $player_id => $total)
-                  <tr class="card-text">
-                    <td>{{ array_search($player_id, array_keys($division_totals)) + 1 }}</td>
-                    <td>{{ \App\User::find($player_id)->name }}</td>
-                    <td>{{ $total }}</td>
-                  </tr>
-                @endforeach
-                
-              </table>
-            </div>
-          </div>
-        @endforeach
-        
-      </div>
-    </div>
-<<<<<<< HEAD
-  </body>
-=======
-  
-    <!--
-      User match history block: Shows the users individual matches,
-      sorted by schedule week (earliest first). The user can see the
-      her match score and her opponent's match score, as well as an
-      edit button that links to the updateScores form.
+        // dd($scoreboard);
+      ?>
       
-      Matches without scores yet reported are clearly marked.
-    -->
+      @foreach ($scoreboard as $division_name => $division_totals)
+        <div class="card border-dark mb-3">
+          <div class="card-header border-dark">{{ $division_name }}</div>
+          <div class="card-body text-dark">
+            <table class="table table-sm">
+              <tr class="card-title">
+                <th>#</th>
+                <th>Player</th>
+                <th>Points</th>
+              </tr>
+              
+              @foreach ($division_totals as $player_id => $total)
+                <tr class="card-text">
+                  <td>{{ array_search($player_id, array_keys($division_totals)) + 1 }}</td>
+                  <td>{{ \App\User::find($player_id)->name }}</td>
+                  <td>{{ $total }}</td>
+                </tr>
+              @endforeach
+              
+            </table>
+          </div>
+        </div>  
+      @endforeach
+    </div>
   </div>
->>>>>>> Modify views to use standard app layout
+</div>
 @endsection
