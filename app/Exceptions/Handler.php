7ad55e5e25217988a5ef3c9acb7723f25eaa9ea4
Creighton Magoun <magoun@gmail.com>
2018-10-06 10:04:43 -0500

    Add error handling for unauthorized access

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -46,6 +46,11 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $exception)
     {
+        if ($exception instanceof \Illuminate\Auth\Access\AuthorizationException) {
+            return redirect('/')
+                ->with('error', $exception->getMessage());  
+        }
+        
         return parent::render($request, $exception);
     }
 }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,17 +8,19 @@
         <title>Racquetball</title>
 
     <link href="https://fonts.googleapis.com/css?family=Raleway:100,300,600" rel="stylesheet" type="text/css">
-        <!-- style -->
+    
         <!--<link href="{{ URL::asset('css/raquetball.css') }}" rel="stylesheet" type="text/css" >-->
         <!-- Bootstrap Stuff -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/css/bootstrap.min.css" integrity="sha384-Smlep5jCw/wG7hdkwQ/Z5nLIefveQRIY9nfy6xoR1uRYBtpZgI6339F5dgvm/e9B" crossorigin="anonymous">
     <!--<link href="/css/style.css" rel="stylesheet">-->
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css">
-    <!--
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    -->
-    <!--<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>-->
-    <!--<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/js/bootstrap.min.js" integrity="sha384-o+RDsa0aLu++PJvFqy8fFScvbHFLtbvScb8AjopnFD+iEQ7wo/CG0xlczd+2O/em" crossorigin="anonymous"></script>-->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/js/bootstrap.min.js" integrity="sha384-o+RDsa0aLu++PJvFqy8fFScvbHFLtbvScb8AjopnFD+iEQ7wo/CG0xlczd+2O/em" crossorigin="anonymous"></script>
+    
+    
+    <!-- Fonts -->
+    
 
     <!-- Styles -->
     
@@ -81,9 +83,29 @@
                     @endauth
                 </div>
             @endif
+            
             <div class="content">
-                <div class="title m-b-md">
-                    Sportsclub Racquetball Flex League
+              @if (\Session::has('error'))
+                <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                  {{ \Session::get('error') }}
+                  <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                  </button>
+                </div>
+              @endif
+              
+              <div class="title m-b-md">
+                Sportsclub Racquetball Flex League
+              </div>
+
+              <div class="container">
+                <div class="card-deck">
+                  <div class="card border-primary mb-3">
+                    <div class="card-header">2018 Fall and Flex Leagues</div>
+                    <div class="card-body text-primary">
+                      <h5 class="card-title">Begins October 1</a></h5>
+                      <p class="card-text">Sign up to Play Singles or Doubles All Skills Welcome</a></p>
+                    <a href ="{{ URL::to('/flyers/2018Racquetfall.pdf') }}"><i class= "fas fa-file-pdf"></i></a>
                     </div>
 
                     <div class="container">
