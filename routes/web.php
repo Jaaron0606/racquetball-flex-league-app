924df34affc9cd7618f886a890c504767e66fa7a
Creighton Magoun <magoun@gmail.com>
2018-10-06 11:30:15 -0500

    Restrict admin dashboard to administrators

--- routes/web.php
+++ routes/web.php
@@ -9,16 +9,20 @@ Route::post('/login/custom', [
   'as' => 'login.custom'
   ]);
 
-Route::group(['middleware'=> 'auth'], function(){
-  
+Route::group(['middleware' => 'auth'], function() {
+  // Logged in users only
   Route::get('/scoreboard', function(){
     return view('scoreboard');
   })->name('scoreboard');
-  
+
+  Route::resource('/users', 'UsersController');
+
+  Route::resource('matches', 'MatchController');
+});
+
+Route::group(['middleware' => 'admin'], function() {
+  // Admins only
   Route::get('/dashboard', function(){
     return view('adminHome');
   })->name('dashboard');
-  
-  Route::resource('matches', 'MatchController');
 });
-Route::resource('/users', 'UsersController');
