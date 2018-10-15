d1eeac06bc96fb8781e490b19a0e6eef09001178
Creighton Magoun <magoun@gmail.com>
2018-10-14 20:29:21 -0500

    Cleanup formatting after rebase

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -69,138 +69,64 @@
     
     </head>
     <body>
-        <div class="flex-center position-ref">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/scoreboard') }}">Scoreboard</a>
-                        <a href="{{ url('/matches') }}">Matches</a>
-                    @else
-                        <a href="{{ route('register') }}">Register</a>
-                        <a href="https://www.sportsclubsc.com/">Sportsclub</a>
-                        <a href="{{ route('login') }}">Player Login</a>
-                        <!--<a href="{{ route('register') }}">Register</a>-->
-                    @endauth
-                </div>
+      <div class="flex-center position-ref">
+          @if (Route::has('login'))
+              <div class="top-right links">
+                  @auth
+                      <a href="{{ url('/scoreboard') }}">Scoreboard</a>
+                      <a href="{{ url('/matches') }}">Matches</a>
+                  @else
+                      <a href="https://www.sportsclubsc.com/">Sportsclub</a>
+                      <a href="{{ route('login') }}">Player Login</a>
+                      <!--<a href="{{ route('register') }}">Register</a>-->
+                  @endauth
+              </div>
+          @endif
+          
+          <div class="content">
+            @if (\Session::has('error'))
+              <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                {{ \Session::get('error') }}
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
             @endif
             
-            <div class="content">
-              @if (\Session::has('error'))
-                <div class="alert alert-danger alert-dismissible fade show" role="alert">
-                  {{ \Session::get('error') }}
-                  <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                  </button>
+            <div class="title m-b-md">
+              Sportsclub Racquetball Flex League
+            </div>
+
+
+            <div class="container">
+            <div class="card-deck">
+              <div class="card border-primary mb-3">
+                <div class="card-header">2018 Fall and Flex Leagues</div>
+                <div class="card-body text-primary">
+                  <h5 class="card-title">Begins October 1</a></h5>
+                  <p class="card-text">Sign up to Play Singles or Doubles All Skills Welcome</a></p>
+                <a href ="{{ URL::to('/flyers/2018Racquetfall.pdf') }}"><i class= "fas fa-file-pdf"></i></a>
                 </div>
-              @endif
-              
-              <div class="title m-b-md">
-                Sportsclub Racquetball Flex League
               </div>
-
-              <div class="container">
-                <div class="card-deck">
-                  <div class="card border-primary mb-3">
-                    <div class="card-header">2018 Fall and Flex Leagues</div>
-                    <div class="card-body text-primary">
-                      <h5 class="card-title">Begins October 1</a></h5>
-                      <p class="card-text">Sign up to Play Singles or Doubles All Skills Welcome</a></p>
-                    <a href ="{{ URL::to('/flyers/2018Racquetfall.pdf') }}"><i class= "fas fa-file-pdf"></i></a>
-                    </div>
-
-                    <div class="container">
-        <div class="card-deck">
-          <div class="card border-primary mb-3">
-            <div class="card-header">2018 Fall and Flex Leagues</div>
-            <div class="card-body text-primary">
-              <h5 class="card-title">Begins October 1</a></h5>
-              <p class="card-text">Sign up to Play Singles or Doubles All Skills Welcome</a></p>
-            <a href ="{{ URL::to('/flyers/2018Racquetfall.pdf') }}"><i class= "fas fa-file-pdf"></i></a>
-            </div>
-          </div>
-          <div class="card border-secondary mb-3">
-            <div class="card-header">2018 Fall and Flex Leagues Standing</div>
-            <div class="card-body text-secondary">
-              <h5 class="card-title">See Standings</h5>
-              <p class="card-text">Click here to see the standing for the 2018 Leagues</p>
-              <a href =""><i class= "fas fa-chart-bar"></i></a>
-            </div>
-          </div>
-          <div class="card border-success mb-3">
-            <div class="card-header">Questions or Comments</div>
-            <div class="card-body text-success">
-              <h5 class="card-title">Please reach out to Lynette Froehlich</h5>
-              <p class="card-text"></p> Send Lynette an email</p>
-            <a href =""><i class="fas fa-envelope"></i></a>
-            </div>
-          </div>
-    
-                    <!-- Extra links -->
-                    <!--<a href="https://laracasts.com">Laracasts</a>-->
-                    <!--<a href="https://laravel-news.com">News</a>-->
-                    <!--<a href="https://forge.laravel.com">Forge</a>-->
-                    <!--<a href="https://github.com/laravel/laravel">GitHub</a>-->
+              <div class="card border-secondary mb-3">
+                <div class="card-header">2018 Fall and Flex Leagues Standing</div>
+                <div class="card-body text-secondary">
+                  <h5 class="card-title">See Standings</h5>
+                  <p class="card-text">Click here to see the standing for the 2018 Leagues</p>
+                  <a href =""><i class= "fas fa-chart-bar"></i></a>
                 </div>
-                
-                Copy
-<div class="card text-white bg-primary mb-3" style="max-width: 18rem;">
-  <div class="card-header">Header</div>
-  <div class="card-body">
-    <h5 class="card-title">Primary card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-  </div>
-</div>
-<div class="card text-white bg-secondary mb-3" style="max-width: 18rem;">
-  <div class="card-header">Header</div>
-  <div class="card-body">
-    <h5 class="card-title">Secondary card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-  </div>
-</div>
-<div class="card text-white bg-success mb-3" style="max-width: 18rem;">
-  <div class="card-header">Header</div>
-  <div class="card-body">
-    <h5 class="card-title">Success card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-  </div>
-</div>
-<div class="card text-white bg-danger mb-3" style="max-width: 18rem;">
-  <div class="card-header">Header</div>
-  <div class="card-body">
-    <h5 class="card-title">Danger card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-  </div>
-</div>
-<div class="card text-white bg-warning mb-3" style="max-width: 18rem;">
-  <div class="card-header">Header</div>
-  <div class="card-body">
-    <h5 class="card-title">Warning card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-  </div>
-</div>
-<div class="card text-white bg-info mb-3" style="max-width: 18rem;">
-  <div class="card-header">Header</div>
-  <div class="card-body">
-    <h5 class="card-title">Info card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-  </div>
-</div>
-<div class="card bg-light mb-3" style="max-width: 18rem;">
-  <div class="card-header">Header</div>
-  <div class="card-body">
-    <h5 class="card-title">Light card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-  </div>
-</div>
-<div class="card text-white bg-dark mb-3" style="max-width: 18rem;">
-  <div class="card-header">Header</div>
-  <div class="card-body">
-    <h5 class="card-title">Dark card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-  </div>
-</div>
+              </div>
+              <div class="card border-success mb-3">
+                <div class="card-header">Questions or Comments</div>
+                <div class="card-body text-success">
+                  <h5 class="card-title">Please reach out to Lynette Froehlich</h5>
+                  <p class="card-text"></p> Send Lynette an email</p>
+                <a href =""><i class="fas fa-envelope"></i></a>
+                </div>
+              </div>
             </div>
-        </div>
+          </div>
+          </div>
         </div>
     </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -1,4 +1,5 @@
 <?php
+Auth::routes();
 
 Route::get('/', function () {
   return view('welcome');
